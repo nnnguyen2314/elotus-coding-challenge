ebaddc5001a3e4e1ec93341f5ca11e00575ba417
Gerard Nguyen <nhatnguyen.nguyen23@gmail.com>
2025-10-25 23:59:41 +0700

    feat: update .env file

--- src/shared/utils/config.ts
+++ src/shared/utils/config.ts
@@ -1,11 +1,10 @@
 export const config = {
-  tmdbBaseUrl: process.env.TMDB_BASE_URL || 'https://api.themoviedb.org/3',
-  // Bearer access token for TMDB API v4
-  tmdbAccessToken: process.env.TMDB_ACCESS_TOKEN || '',
-  imageBaseUrl: process.env.TMDB_MOVIE_IMAGE_BASE_URL || 'https://image.tmdb.org/t/p',
+    tmdbBaseUrl: process.env.TMDB_BASE_URL || 'https://api.themoviedb.org/3',
+    tmdbAccessToken: process.env.TMDB_ACCESS_TOKEN || '',
+    imageBaseUrl: process.env.TMDB_MOVIE_IMAGE_BASE_URL || 'https://image.tmdb.org/t/p',
 };
 
 export const getImageUrl = (path?: string | null, size: 'w92'|'w200'|'w300'|'w500'|'original' = 'w300') => {
-  if (!path) return '';
-  return `${config.imageBaseUrl}/${size}${path}`;
+    if (!path) return '';
+    return `${config.imageBaseUrl}/${size}${path}`;
 };
