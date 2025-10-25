8dccd537465c04b9e3c0052066831ff89b03b7fb
Gerard Nguyen <nhatnguyen.nguyen23@gmail.com>
2025-10-26 00:17:44 +0700

    feat: update config file

--- src/shared/utils/config.ts
+++ src/shared/utils/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   tmdbBaseUrl: process.env.TMDB_BASE_URL || 'https://api.themoviedb.org/3',
   // Bearer access token for TMDB API v4
-  tmdbAccessToken: process.env.TMDB_ACCESS_TOKEN || '',
+  tmdbAccessToken: process.env.TMDB_ACCESS_TOKEN || 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmY0ZTI1NDJiOTRmNzk0NTY2MTRmMmM3MTY3ZTM5MSIsIm5iZiI6MTc2MTMxODg3MC44MzcsInN1YiI6IjY4ZmI5N2Q2ZWM2OWYzMTg0ODA5NDgyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zrx5esiGl5-DjwVeRKkzEQ7qJQXYClC6q-H7Vr0R2l0',
   imageBaseUrl: process.env.TMDB_MOVIE_IMAGE_BASE_URL || 'https://image.tmdb.org/t/p',
 };
 
